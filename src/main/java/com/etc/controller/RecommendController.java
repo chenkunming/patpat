df8ab8a4df655d3ad343aa95776ed8f0ac5f543e
chenkunming <302900395@qq.com>
2018-07-25 13:46:03 +0800

    推荐功能相关

--- src/main/java/com/etc/controller/RecommendController.java
+++ src/main/java/com/etc/controller/RecommendController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+//推荐controller
 
 @RestController
 public class RecommendController {
